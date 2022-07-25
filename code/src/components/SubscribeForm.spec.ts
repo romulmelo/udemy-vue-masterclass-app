09d98421f90363508164eac2ea3b507b5b1ee61e
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-07-25 13:51:49 -0300

    refactor: mount SUT before tests

--- code/src/components/SubscribeForm.spec.ts
+++ code/src/components/SubscribeForm.spec.ts
@@ -1,18 +1,22 @@
-import { mount } from "@vue/test-utils"
+import { mount, VueWrapper } from "@vue/test-utils"
 
 import SubscribeForm from "./SubscribeForm.vue"
 
 describe("SubscribeForm", () => {
+  let sut: VueWrapper
+
+  beforeEach(() => {
+    sut = mount(SubscribeForm)
+  })
+
   test("should display a title", () => {
-    const wrapper = mount(SubscribeForm)
-    const title = wrapper.find('[data-test="form-title"]')
+    const title = sut.find('[data-test="form-title"]')
 
     expect(title.text()).toMatch("Subscribe to our newsletter!")
   })
 
   test("should display a description", () => {
-    const wrapper = mount(SubscribeForm)
-    const description = wrapper.find('[data-test="form-description"]')
+    const description = sut.find('[data-test="form-description"]')
 
     expect(description.text()).toMatch(
       "We'll send you the best of our blog just once a month. We promise."
@@ -20,8 +24,7 @@ describe("SubscribeForm", () => {
   })
 
   test("should display an input for email subscription", () => {
-    const wrapper = mount(SubscribeForm)
-    const input = wrapper.find('input[type="email"]')
+    const input = sut.find('input[type="email"]')
 
     expect(input.exists()).toBe(true)
     expect(input.attributes("type")).toBe("email")
@@ -30,8 +33,7 @@ describe("SubscribeForm", () => {
   })
 
   test("should display a button to subscribe", () => {
-    const wrapper = mount(SubscribeForm)
-    const button = wrapper.find('button[type="submit"]')
+    const button = sut.find('button[type="submit"]')
 
     expect(button.exists()).toBe(true)
     expect(button.text()).toBe("Subscribe")
@@ -39,52 +41,46 @@ describe("SubscribeForm", () => {
   })
 
   test("should disabled button if input is empty", async () => {
-    const wrapper = mount(SubscribeForm)
-    const button = wrapper.find('button[type="submit"]')
+    const button = sut.find('button[type="submit"]')
 
-    await wrapper.find('input[type="email"]').setValue("")
+    await sut.find('input[type="email"]').setValue("")
 
     expect(button.attributes("disabled")).toBe("true")
   })
 
   test("should display an error if email is invalid", async () => {
-    const wrapper = mount(SubscribeForm)
-    const input = wrapper.find<HTMLInputElement>("input[type='email']")
+    const input = sut.find<HTMLInputElement>("input[type='email']")
     const regex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
 
     await input.setValue("invalid email")
 
     expect(input.element.value.match(regex)).toBeFalsy()
-    expect(wrapper.find('[data-test="form-error"]').exists()).toBe(true)
-    expect(wrapper.find('[data-test="form-error"]').text()).toMatch(
-      "Invalid email"
-    )
+    expect(sut.find('[data-test="form-error"]').exists()).toBe(true)
+    expect(sut.find('[data-test="form-error"]').text()).toMatch("Invalid email")
   })
 
   test("should register email on submit", async () => {
-    const wrapper = mount(SubscribeForm)
     const email = "name@mail.com"
 
-    await wrapper.find("input[type=email]").setValue(email)
+    await sut.find("input[type=email]").setValue(email)
 
-    await wrapper.find("form").trigger("submit.prevent")
+    await sut.find("form").trigger("submit.prevent")
 
-    expect(wrapper.emitted("submit")?.[0][0]).toStrictEqual({
+    expect(sut.emitted("submit")?.[0][0]).toStrictEqual({
       email,
       subscribe: true
     })
   })
 
   test("should clear input after submit", async () => {
-    const wrapper = mount(SubscribeForm)
     const email = "name@mail.com"
 
-    await wrapper.find("input[type=email]").setValue(email)
+    await sut.find("input[type=email]").setValue(email)
 
-    await wrapper.find("form").trigger("submit.prevent")
+    await sut.find("form").trigger("submit.prevent")
 
-    expect(
-      wrapper.find<HTMLInputElement>("input[type=email]").element.value
-    ).toBe("")
+    expect(sut.find<HTMLInputElement>("input[type=email]").element.value).toBe(
+      ""
+    )
   })
 })
